c2891044dc6d7aabb14214e30ecc88e0a6956649
mdNaimulHoque <naimulhoq89@gmail.com>
2020-10-01 00:03:58 +0600

    Updates

---
+++ precache-manifest.e1a8141043cb8eb76ae70e4faad77481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5050f48dff680b79f79db83b363c448",
+    "url": "/Assignment-8/index.html"
+  },
+  {
+    "revision": "6dacc71fde0bae8dfbeb",
+    "url": "/Assignment-8/static/css/main.5e5c8fb4.chunk.css"
+  },
+  {
+    "revision": "e442c3838d408911f128",
+    "url": "/Assignment-8/static/js/2.65721c56.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/Assignment-8/static/js/2.65721c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dacc71fde0bae8dfbeb",
+    "url": "/Assignment-8/static/js/main.9e08caf9.chunk.js"
+  },
+  {
+    "revision": "105b8e46470a42c13169",
+    "url": "/Assignment-8/static/js/runtime-main.86f35b06.js"
+  }
+]);
